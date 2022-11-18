86326b55db9a2c988bff7f38e4a4ffb4a6ba91ca
AnastasiaBokinsky <bokinskymail@gmail.com>
2022-11-17 21:59:38 -0500

    bootstrap added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="conteiner">
+        <div className="col-3">
+          <h1>Hi there</h1>
+          <button type="button" class="btn btn-primary">
+            Primary
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
