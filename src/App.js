f7c69ede4125bf8cd908feb51f1a6a84be43d0e2
AnastasiaBokinsky <bokinskymail@gmail.com>
2022-11-18 17:28:18 -0500

    axios, API installed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="conteiner">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Anastasiia Boiko and it is{" "}
           <a
